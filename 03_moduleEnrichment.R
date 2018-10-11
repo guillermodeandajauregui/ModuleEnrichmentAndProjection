6830d3e32caf7dcb10b0fedc07e4a817c2895acc
gdj_porg <x@desde.porg.com>
2018-10-11 16:09:37 -0500

    added controls to enrichment step

--- 03_moduleEnrichment.R
+++ 03_moduleEnrichment.R
@@ -73,3 +73,66 @@ bp_cases = bipartite_projection(graph = b_cases, which = TRUE)
 
 write.graph(graph = bp_cases, file = "results/bp_cases.gml", "gml")
 
+#cntrl
+#get list of communities 
+l_comm_cntrl = communities(modules_cntrl)
+#in case we are provided with communities already identified for each vertex
+## tmp_df = get.data.frame(g, "vertices")
+## tmp_l  = lapply(X = unique(tmp_df$infomap), FUN = function(i){
+##   filter(tmp_df, infomap==i)$name
+## })
+
+#Enrich
+
+enrichment_list_cntrl = parallel::mclapply(X = seq_along(l_comm_cntrl), mc.cores = 4, FUN = function(i){
+  #enrichment_list_cntrl = lapply(X = seq_along(l_comm_cntrl), FUN = function(i){
+  
+  nomen = names(l_comm)[i]
+  
+  my_enrichment = HTSanalyzeR::multiHyperGeoTest(collectionOfGeneSets = LIST_GO, 
+                                                 universe = V(g)$name, 
+                                                 hits = l_comm[[i]], 
+                                                 minGeneSetSize = 1, 
+                                                 pAdjustMethod = "BH", 
+                                                 verbose = TRUE
+  )
+  print(nrow(my_enrichment))
+  my_2 = tibble::rownames_to_column(as.data.frame(my_enrichment))#%>%filter(Adjusted.Pvalue<10)
+  my_3 = cbind(comm = nomen, my_2)
+  return(my_3)
+  
+})
+
+enrichment_df_cntrl = ldply(enrichment_list_cntrl, data.frame)
+#calculate adjusted pvalue for all communities
+enrichment_df_cntrl$Adjusted.Pvalue2 = p.adjust(enrichment_df_cntrl$Pvalue, method = "BH")
+#which(enrichment_df_cntrl$Adjusted.Pvalue<0.05)
+#which(enrichment_df_cntrl$Adjusted.Pvalue2<0.05)
+
+
+#bipartite graph
+b_cntrl = graph_from_data_frame(enrichment_df_cntrl, directed = FALSE)
+b_cntrl = delete.edges(graph = b_cntrl, 
+                       edges = E(b_cntrl)[Adjusted.Pvalue2>pvalue_threshold]
+)
+
+V(b_cntrl)$type = TRUE
+V(b_cntrl)$type[grep(pattern = "GO_", x = V(b_cntrl)$name)] = FALSE
+
+#add a value that tells us if the comm shows enrichment or not
+#if it has neighbors, it enriches. Else, no
+V(b_cntrl)$enrich = degree(b_cntrl)
+V(b_cntrl)$enrich = ifelse(V(b_cntrl)$enrich==0, "YES", "NO")
+
+#write out bipartite
+write.graph(b_cntrl, file = "results/bipartite_cntrl.gml", "gml")
+
+#4) Enrichment projection
+
+
+
+#projection
+bp_cntrl = bipartite_projection(graph = b_cntrl, which = TRUE)
+
+write.graph(graph = bp_cntrl, file = "results/bp_cntrl.gml", "gml")
+
